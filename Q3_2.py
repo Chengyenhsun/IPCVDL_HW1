060ef0748c76b803f7313d8698387eafe57c8ce1
Iris <f14106155@gs.ncku.edu.tw>
2023-11-04 00:47:19 +0800

    完成Q3_2

---
+++ Q3_2.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+
+Q3_image_path = "Dataset_OpenCvDl_Hw1/Q3_image/"
+
+# 讀取彩色圖片
+imageq32 = cv2.imread(Q3_image_path + "building.jpg")
+
+# 步驟1：將RGB圖像轉換為灰度圖像
+grayq32 = cv2.cvtColor(imageq32, cv2.COLOR_BGR2GRAY)
+
+
+# 調整平滑程度的核大小
+def apply_gaussian_blur(image, kernel_size):
+    blurred = cv2.GaussianBlur(image, (kernel_size, kernel_size), 0)
+    return blurred
+
+
+# 步驟2：使用高斯平滑濾波器對灰度圖像進行平滑處理
+kernel_size = 5
+smoothed_image2 = apply_gaussian_blur(grayq32, kernel_size)
+
+
+# 步驟3：使用Sobel x運算子進行邊緣檢測
+sobel_y = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]], dtype=np.float32)
+sobel_y_image = cv2.filter2D(smoothed_image2, -1, sobel_y)
+
+
+# 步驟4：顯示結果
+cv2.imshow("sobel x", sobel_y_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
