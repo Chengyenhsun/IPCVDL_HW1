0cd23a3edaf1f6d04bc04f4e1ab70d12f3b255fc
Iris <f14106155@gs.ncku.edu.tw>
2023-11-05 03:13:55 +0800

    加入Q2_1~Q2_3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 Q1_image_path = "Dataset_OpenCvDl_Hw1/Q1_image/"
+Q2_image_path = "Dataset_OpenCvDl_Hw1/Q2_image/"
 Q3_image_path = "Dataset_OpenCvDl_Hw1/Q3_image/"
 
 
@@ -49,6 +50,103 @@ def Q1_2():
     cv2.imshow("I1", I1)
     cv2.imshow("I2", I2)
     cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def Q2_1():
+    # 讀取彩色圖片
+    image = cv2.imread(Q2_image_path + "image1.jpg")
+
+    # 設定初始的半徑大小
+    radius = 0
+
+    # 創建一個函數，用於更新圖像
+    def update_image(value):
+        global radius
+        radius = value + 1
+        # 獲取trackbar的當前值
+        radius = cv2.getTrackbarPos("Radius", "Gaussian Blur")
+
+        # 計算kernel的大小
+        kernel_size = (2 * radius + 1, 2 * radius + 1)
+
+        # 運用高斯濾波
+        blurred_image = cv2.GaussianBlur(image, kernel_size, 0)
+
+        # 顯示結果
+        cv2.imshow("Gaussian Blur", blurred_image)
+
+    cv2.namedWindow("Gaussian Blur")
+
+    cv2.createTrackbar("Radius", "Gaussian Blur", 0, 5, update_image)
+
+    cv2.imshow("Gaussian Blur", image)
+
+    # 等待用戶按下任意按鍵
+    cv2.waitKey(0)
+
+    # 釋放資源
+    cv2.destroyAllWindows()
+
+
+def Q2_2():
+    # 讀取彩色圖片
+    image = cv2.imread(Q2_image_path + "image1.jpg")
+
+    # 初始化窗口半徑
+    radius = 0
+
+    # 設定sigmaColor和sigmaSpace的值
+    sigmaColor = 90
+    sigmaSpace = 90
+
+    # 創建一個空視窗
+    cv2.namedWindow("Bilateral Filter")
+
+    # 創建一個回呼函數，當軌跡條值改變時調用
+    def update_radius(value):
+        global radius
+        radius = value + 1
+        # 使用Bilateral Filter處理圖片
+        filtered_image = cv2.bilateralFilter(
+            image, (2 * radius + 1), sigmaColor, sigmaSpace
+        )
+        # 顯示處理後的圖片
+        cv2.imshow("Bilateral Filter", filtered_image)
+
+
+def Q2_3():
+    # 讀取彩色圖片
+    image = cv2.imread(Q2_image_path + "image2.jpg")
+
+    # 初始化窗口半徑
+    radius = 0
+
+    # 創建一個空視窗
+    cv2.namedWindow("Median Filter")
+
+    # 回呼函數，當軌跡條值改變時調用
+    def update_radius(value):
+        global radius
+        radius = value + 1
+        # 計算kernel大小
+        kernel_size = 2 * radius + 1
+        # 使用Median Filter處理圖片
+        filtered_image = cv2.medianBlur(image, kernel_size)
+        # 顯示處理後的圖片
+        cv2.imshow("Median Filter", filtered_image)
+
+    # 創建軌跡條，用於調整半徑大小
+    cv2.createTrackbar("半徑", "Median Filter", 0, 5, update_radius)
+
+    # 顯示初始處理後的圖片
+    filtered_image = cv2.medianBlur(image, 2 * radius + 1)
+    cv2.imshow("Median Filter", filtered_image)
+
+    # 等待用戶按下任意按鍵
+    cv2.waitKey(0)
+
+    # 釋放資源
     cv2.destroyAllWindows()
 
 
