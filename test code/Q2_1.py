ea8fad80c8a3822bf20aa662a9795eb11583c8aa
Iris <f14106155@gs.ncku.edu.tw>
2023-11-05 01:42:23 +0800

    修改Q2_1

--- test code/Q2_1.py
+++ test code/Q2_1.py
@@ -8,35 +8,29 @@ Q2_image_path = "Dataset_OpenCvDl_Hw1/Q2_image/"
 image = cv2.imread(Q2_image_path + "image1.jpg")
 
 
-# 创建一个函数，用于更新图像
+# 創建一個函數，用於更新圖像
 def update_image(*args):
-    # 获取trackbar的当前值
+    # 獲取trackbar的當前值
     radius = cv2.getTrackbarPos("Radius", "Gaussian Blur")
 
-    # 计算核的大小
+    # 計算kernel的大小
     kernel_size = (2 * radius + 1, 2 * radius + 1)
 
-    # 应用高斯滤波
+    # 運用高斯濾波
     blurred_image = cv2.GaussianBlur(image, kernel_size, 0)
 
-    # 在弹出窗口中显示结果
+    # 顯示結果
     cv2.imshow("Gaussian Blur", blurred_image)
 
 
-# 创建弹出窗口
 cv2.namedWindow("Gaussian Blur")
 
-# 创建trackbar
 cv2.createTrackbar("Radius", "Gaussian Blur", 1, 5, update_image)
 
-# 初始显示原始图像
 cv2.imshow("Gaussian Blur", image)
 
-while True:
-    key = cv2.waitKey(1)
+# 等待用戶按下任意按鍵
+cv2.waitKey(0)
 
-    # 按ESC键退出
-    if key == 27:
-        break
-
+# 釋放資源
 cv2.destroyAllWindows()
