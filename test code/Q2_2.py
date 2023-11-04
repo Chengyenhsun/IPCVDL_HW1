6629804f22df293644c08ec35ecff7b1c00ffaab
Iris <f14106155@gs.ncku.edu.tw>
2023-11-05 03:09:51 +0800

    修改Q2_2(上一個為修改Q2_1)

--- test code/Q2_2.py
+++ test code/Q2_2.py
@@ -6,9 +6,8 @@ Q2_image_path = "Dataset_OpenCvDl_Hw1/Q2_image/"
 # 讀取彩色圖片
 image = cv2.imread(Q2_image_path + "image1.jpg")
 
-
-# 設定初始的半徑大小
-radius = 1
+# 初始化窗口半徑
+radius = 0
 
 # 設定sigmaColor和sigmaSpace的值
 sigmaColor = 90
@@ -21,7 +20,7 @@ cv2.namedWindow("Bilateral Filter")
 # 創建一個回呼函數，當軌跡條值改變時調用
 def update_radius(value):
     global radius
-    radius = value
+    radius = value + 1
     # 使用Bilateral Filter處理圖片
     filtered_image = cv2.bilateralFilter(
         image, (2 * radius + 1), sigmaColor, sigmaSpace
@@ -31,7 +30,7 @@ def update_radius(value):
 
 
 # 創建一個軌跡條，用於調整半徑大小
-cv2.createTrackbar("Radius", "Bilateral Filter", 1, 5, update_radius)
+cv2.createTrackbar("Radius", "Bilateral Filter", 0, 5, update_radius)
 
 # 顯示初始的處理後圖片
 filtered_image = cv2.bilateralFilter(image, (2 * radius + 1), sigmaColor, sigmaSpace)
