817dcf89a0051c2b9576480cb99f66f623742d81
Iris <f14106155@gs.ncku.edu.tw>
2023-11-05 11:42:19 +0800

    修改Q2_2

--- main.py
+++ main.py
@@ -53,6 +53,36 @@ def Q1_2():
     cv2.destroyAllWindows()
 
 
+def Q1_3():
+    # 讀取彩色圖片
+    image = cv2.imread(Q1_image_path + "rgb.jpg")
+
+    # 步驟1: 轉換圖像為HSV格式
+    hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # 步驟2: 提取黃色和綠色的遮罩，生成I1
+    lower_yellow = np.array([12, 43, 43])  # HSV中黄色的下限值
+    upper_yellow = np.array([35, 255, 255])  # HSV中黄色的上限值
+    lower_green = np.array([35, 43, 46])  # HSV中绿色的下限值
+    upper_green = np.array([77, 255, 255])  # HSV中绿色的上限值
+    mask_yellow = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
+    mask_green = cv2.inRange(hsv_img, lower_green, upper_green)
+    mask_i1 = mask_yellow + mask_green
+
+    # 步驟3: 將黃色和綠色的遮罩轉成BGR格式
+    mask_i1_bgr = cv2.cvtColor(mask_i1, cv2.COLOR_GRAY2BGR)
+    mask = cv2.bitwise_not(mask_i1_bgr)
+
+    # 步驟4: 從圖像中移除黃色和綠色，生成I2
+    i2 = cv2.bitwise_and(mask, image)
+
+    # 顯示 I1 及 I2
+    cv2.imshow("I1", mask_i1)
+    cv2.imshow("I2", i2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 def Q2_1():
     # 讀取彩色圖片
     image = cv2.imread(Q2_image_path + "image1.jpg")
@@ -77,15 +107,11 @@ def Q2_1():
         cv2.imshow("Gaussian Blur", blurred_image)
 
     cv2.namedWindow("Gaussian Blur")
-
     cv2.createTrackbar("Radius", "Gaussian Blur", 0, 5, update_image)
-
     cv2.imshow("Gaussian Blur", image)
 
     # 等待用戶按下任意按鍵
     cv2.waitKey(0)
-
-    # 釋放資源
     cv2.destroyAllWindows()
 
 
@@ -113,6 +139,15 @@ def Q2_2():
         )
         # 顯示處理後的圖片
         cv2.imshow("Bilateral Filter", filtered_image)
+
+    # 創建一個軌跡條，用於調整半徑大小
+    cv2.createTrackbar("Radius", "Bilateral Filter", 0, 5, update_radius)
+
+    # 等待用戶按下任意按鍵
+    cv2.waitKey(0)
+
+
+cv2.destroyAllWindows()
 
 
 def Q2_3():
@@ -145,8 +180,6 @@ def Q2_3():
 
     # 等待用戶按下任意按鍵
     cv2.waitKey(0)
-
-    # 釋放資源
     cv2.destroyAllWindows()
 
 
@@ -202,4 +235,4 @@ def Q3_2():
     cv2.destroyAllWindows()
 
 
-Q3_2()
+Q2_2()
