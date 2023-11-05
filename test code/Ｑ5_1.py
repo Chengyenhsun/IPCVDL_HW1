632d063bdef5a35bb07ffdb3678cc486ed770107
Iris <f14106155@gs.ncku.edu.tw>
2023-11-05 16:00:19 +0800

    修改Q5_1

--- test code/Ｑ5_1.py
+++ test code/Ｑ5_1.py
@@ -1,9 +1,12 @@
 import os
 from PIL import Image
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+Q5_image_path = "Dataset_OpenCvDl_Hw1/Q5_image/"
 
 # 定義圖像路徑
-image_folder = "Dataset_OpenCvDl_Hw1/Q5_image/Q5_1/"
+image_folder = Q5_image_path + "Q5_1"
 
 # 載入圖像
 image_files = [
@@ -22,6 +25,16 @@ transform = transforms.Compose(
 
 augmented_images = [transform(image) for image in images]
 
-# 顯示增強後的圖像
-for i, augmented_image in enumerate(augmented_images):
-    augmented_image.show()
+# 提取檔名（不包含格式）作為標籤
+labels = [os.path.splitext(os.path.basename(file))[0] for file in image_files]
+
+# 顯示增強後的圖像和標籤在一個新視窗中
+fig, axes = plt.subplots(3, 3, figsize=(12, 4))
+
+for i, (original, augmented, label) in enumerate(zip(images, augmented_images, labels)):
+    ax = axes[i // 3, i % 3]
+    ax.set_title(label)
+    ax.imshow(original if i % 3 == 0 else augmented)
+    ax.axis("off")
+
+plt.show()
