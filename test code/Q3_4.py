25bf5d69a13992173336f0240d78454e8b4ccbee
ChenSheeep <apple.inc.taiwan@gmail.com>
2023-11-06 22:10:12 +0800

    修改Q3_4 (還是怪怪的)

--- test code/Q3_4.py
+++ test code/Q3_4.py
@@ -35,9 +35,26 @@ for y in range(1, smoothed_image.shape[0] - 1):
 sobel_x_image = np.clip(sobel_x_image, 0, 255).astype(np.uint8)
 sobel_y_image = np.clip(sobel_y_image, 0, 255).astype(np.uint8)
 
+gx = np.zeros(sobel_x_image.shape, dtype=np.uint8)
+gy = np.zeros(sobel_y_image.shape, dtype=np.uint8)
+gxy = np.zeros(sobel_x_image.shape, dtype=np.uint8)
+
+for h in range(1, sobel_x_image.shape[0] - 1):
+    for w in range(1, sobel_x_image.shape[1] - 1):
+        sx = sobel_x_image[h, w]
+        sy = sobel_y_image[h, w]
+
+        sxy = int(np.round(np.sqrt(sx**2 + sy**2)))
+
+        gx[h, w] = np.clip(sx, 0, 255)
+        gy[h, w] = np.clip(sy, 0, 255)
+        gxy[h, w] = np.clip(sxy, 0, 255)
+
+
 # 計算梯度角度
 gradient_angle = np.arctan2(sobel_y_image, sobel_x_image) * 180 / np.pi
-# gradient_angle = (gradient_angle + 180) * 255 / 360
+# Convert gradient_angle to 8-bit integer
+gradient_angle = ((gradient_angle + 180) * 255 / 360).astype(np.uint8)
 print("Gradient Angle:\n", gradient_angle)
 
 # # 歸一化梯度角度到0~360度範圍
@@ -51,9 +68,24 @@ print("Gradient Angle:\n", gradient_angle)
 
 # Show the mask for the specified angle range
 # cv2.imshow("Mask (120-180 degrees)", angle_range_mask)
-# # 生成兩個不同範圍的角度掩碼
-# mask1 = ((gradient_angle >= 120) & (gradient_angle <= 180)).astype(np.uint8) * 255
-# mask2 = ((gradient_angle >= 210) & (gradient_angle <= 330)).astype(np.uint8) * 255
-cv2.imshow("Gradient Angle", gradient_angle)
+
+# 生成兩個不同範圍的角度掩碼
+mask1 = ((gradient_angle >= 120) & (gradient_angle <= 180)).astype(np.uint8) * 255
+mask2 = ((gradient_angle >= 210) & (gradient_angle <= 330)).astype(np.uint8) * 255
+# mask1 = cv2.inRange(gradient_angle, 120, 180)
+# mask2 = cv2.inRange(gradient_angle, 210, 330)
+
+
+result1 = cv2.bitwise_and(gxy, gxy, mask=mask1)
+result2 = cv2.bitwise_and(gxy, gxy, mask=mask2)
+# fig = plt.figure(figsize=(10,5))
+# plt.subplot(1, 2, 1), plt.title('120~180')
+# plt.imshow(result1,cmap=plt.get_cmap('gray')), plt.axis('off')
+# plt.subplot(1,2, 2), plt.title('210~330')
+# plt.imshow(result2,cmap=plt.get_cmap('gray')), plt.axis('off')
+
+
+cv2.imshow("result1", result1)
+cv2.imshow("result2", result2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
